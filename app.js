dfd02772c16e79c7184ede38e8ff47e21a73e469
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2019-01-08 00:54:38 +0100

    Data creation (unfinished)

--- app.js
+++ app.js
@@ -23,6 +23,21 @@ var budgetController = (function(){
         exp: 0,
         inc: 0
     }
+   };
+   return {
+       addItem: function(type, des, val){
+            var newItem;
+
+            ID = 0;
+
+            if (type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc'){
+                newItem = new Income(ID, des, val);
+            }
+
+            data.allItems[type].push
+       }
    }
  })();
 
