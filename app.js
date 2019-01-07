2b3dd9fed656152b810f5b2489a2a77148617f59
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2019-01-07 23:32:04 +0100

    Added controllers

---
+++ app.js
@@ -0,0 +1,64 @@
+// DATA CONTROLLER
+
+var budgetController = (function(){
+
+    //code
+
+ })();
+
+
+ // UI CONTROLLER 
+
+ var UIController = (function(){
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
+
+    return {
+
+        getInput: function(){
+
+            return {
+                 type: document.querySelector(DOMstrings.inputType).value,
+                 description: document.querySelector(DOMstrings.inputDescription).value,
+                 value: document.querySelector(DOMstrings.inputValue).value
+            };
+
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
+
+ })();
+
+ // APP CONTROLLER
+
+ var controller = (function(budgetCtrl, UICtrl){
+
+    var DOM = UICtrl.getDOMstrings();
+
+    var ctrlAddItem = function(){
+        // GET DATA
+        var input = UICtrl.getInput();
+        console.log(input);
+        // ADD ITEM TO BUDGET CONTROLLER
+        // ADD ITEM TO UI
+        // CALCULATE BUDGET
+        // DISPLAY BUDGET
+    };
+    
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(e){
+        if (e.keyCode === 13 || e.which === 13){
+            ctrlAddItem();
+        }
+    });
+
+ })(budgetController, UIController);
